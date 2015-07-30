45828bae7c3e3184c5a9e9022d7ea502f0db3e97
kirkl <kirkl@vulcan.com>
2015-07-30 11:14:56 -0700

    expanded bethic to include community and substrate

--- global_finprint/benthos/models.py
+++ global_finprint/benthos/models.py
@@ -6,6 +6,22 @@ class BenthicCategory(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
 
+class BenthicCommunity(models.Model):
+    # todo:  placeholder
+    name = models.CharField(max_length=100, unique=True)
+
+
+class Substrate(models.Model):
+    # todo:  placeholder
+    name = models.CharField(max_length=100, unique=True)
+
+
+class Benthic(models.Model):
+    # todo:  placeholder
+    name = models.CharField(max_length=100, unique=True)
+    category = models.ForeignKey(to=BenthicCategory)
+    community = models.ForeignKey(to=BenthicCommunity)
+    substrate = models.ForeignKey(to=Substrate)
 
 
 
