d9fd5138f58b38886c3078b18f91440f797487fe
Stuartg <jaxiano@gmail.com>
2015-09-06 08:50:35 -0700

    Clean up trip form.

--- global_finprint/trip/forms.py
+++ global_finprint/trip/forms.py
@@ -1,19 +1,22 @@
 from django.forms import ModelForm
+from django.forms.formsets import formset_factory
 from global_finprint.trip import models
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit, HTML
+from crispy_forms.layout import Submit, HTML, Layout, Fieldset
 from crispy_forms.bootstrap import FormActions
 
-class TripForm(ModelForm):
 
+class TripForm(ModelForm):
     class Meta:
         model = models.Trip
-        fields = ['name', 'location', 'start_datetime', 'end_datetime', 'team', 'boat', 'type']
+        fields = ['team', 'start_date', 'end_date', 'location', 'boat', 'type']
 
     def __init__(self, *args, **kwargs):
-        super(ModelForm, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.helper = FormHelper(self)
-        self.helper.layout.append(FormActions(
-                    HTML("""<a role="button" class="btn btn-default"
-                         href="{% url "trip_list" %}">Cancel</a>"""),
-                    Submit('save', 'Submit'),))
+        self.helper.form_action = "{{ action }}"
+        self.helper.form_method = "post"
+        self.helper.layout.append(
+            FormActions(HTML("""<a role="button" class="btn btn-default" href="{% url "trip_list" %}">Cancel</a>"""),
+                        Submit('save', 'Save')))
+
