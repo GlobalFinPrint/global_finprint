4f5cd6f22f9ebb6b603f498b06db8691169f87d1
Stuartg <jaxiano@gmail.com>
2015-09-08 16:24:35 -0700

    Register more models for admin page (although benthic no worky...hmmm)

--- global_finprint/trip/admin.py
+++ global_finprint/trip/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from global_finprint.trip.models import Trip, Location, Reef, Team
+from global_finprint.trip.models import Trip, Location, Reef, Team, Site
 
 admin.site.register(Trip)
 admin.site.register(Location)
 admin.site.register(Reef)
 admin.site.register(Team)
+admin.site.register(Site)
