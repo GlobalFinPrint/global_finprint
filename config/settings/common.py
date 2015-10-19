ce3fef0c8bca76e677a65295f276e876f219bedb
vkirkl <kirkl@vulcan.com>
2015-10-18 19:53:04 -0700

    added a datetime picker widget.

--- config/settings/common.py
+++ config/settings/common.py
@@ -40,6 +40,7 @@ THIRD_PARTY_APPS = (
     #'allauth',  # registration
     #'allauth.account',  # registration
     #'allauth.socialaccount',  # registration
+    'bootstrap3_datetime',
 )
 
 # Apps specific for this project go here.
