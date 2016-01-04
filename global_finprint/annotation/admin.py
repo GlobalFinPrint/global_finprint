c923173a73163c6153b5f4a2da8500264e2798c0
Robert Reis <T-RobertR@vulcan.com>
2016-01-04 14:09:05 -0800

    remove annotator from admin screen since its a user now

--- global_finprint/annotation/admin.py
+++ global_finprint/annotation/admin.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from global_finprint.annotation import models
 
 
-admin.site.register(models.Annotator)
 admin.site.register(models.AnimalBehavior)
 admin.site.register(models.AnimalGroup)
 admin.site.register(models.Animal)
