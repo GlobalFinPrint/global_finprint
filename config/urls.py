313637cdf7125eb31dfb3cc4c4285d532354b8a3
vkirkl <kirkl@vulcan.com>
2016-01-29 11:28:10 -0800

    enable password change

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,9 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import login, logout, password_change, password_change_done
+from django.contrib.auth.forms import PasswordChangeForm
 from django.views.defaults import bad_request, permission_denied, page_not_found, server_error
 
 from global_finprint.core.views import UrlRedirect
@@ -29,6 +31,12 @@ urlpatterns = [
     # User management
     url(r'^accounts/login/$', login, {'template_name': 'registration/login.html'}, name='finprint_login'),
     url(r'^accounts/logout/$', logout, {'template_name': 'registration/logged_out.html'}, name='finprint_logout'),
+    url(r'^accounts/password_change/$', login_required(password_change),
+        {'password_change_form': PasswordChangeForm},
+        name='password_change'),
+    url(r'^accounts/password_change_done/$', login_required(password_change_done),
+        {'template_name': 'registration/password_change_done.html'},
+        name='password_change_done'),
     url(r'^accounts/profile/$', UrlRedirect.as_view()),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
