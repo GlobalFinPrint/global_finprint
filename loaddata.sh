0659b5ac0a1343546aff25c3f2c5c5236638aef9
vkirkl <kirkl@vulcan.com>
2016-01-30 11:08:52 -0800

    initial values for all core FP and Vuclan users.

--- loaddata.sh
+++ loaddata.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-# create admin
-python manage.py createsuperuser
-
 # load base lookup data
+python manage.py loaddata global_finprint/core/fixtures/core_users
 python manage.py loaddata global_finprint/habitat/fixtures/habitat_locations
 python manage.py loaddata global_finprint/annotation/fixtures/annotation_animal
 python manage.py loaddata global_finprint/bruv/fixtures/bruv_equipment
