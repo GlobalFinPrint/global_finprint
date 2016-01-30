f6fcb5482f340b38dc422e53f8aa4ce3df6f0569
vkirkl <kirkl@vulcan.com>
2016-01-30 12:48:09 -0800

    build trip code if not present.

--- global_finprint/trip/models.py
+++ global_finprint/trip/models.py
@@ -19,12 +19,21 @@ class Team(AuditableModel):
 class Trip(AuditableModel):
     # suggested code pattern:
     # FP_[location.code]_[start_date.year]_[trip number within location / year]
-    code = models.CharField(max_length=32, help_text='FP_[year]_[loc code]_xx')  # todo:  unique??
+    code = models.CharField(max_length=32, help_text='FP_[year]_[loc code]_xx', null=True, blank=True)  # todo:  unique??
     team = models.ForeignKey(Team)
     location = models.ForeignKey(Location)
     boat = models.CharField(max_length=100, blank=True, null=True)
     start_date = models.DateField()
     end_date = models.DateField()
+
+    def save(self, *args, **kwargs):
+        if not self.code:  # trip code if it hasn't been set:  FP_[year]_[loc code]_xx
+            self.code = u'FP_{}_{}_xx'.format(self.start_date.year, self.location.code)
+        super(Trip, self).save(*args, **kwargs)
+        self.refresh_from_db()
+        next_id = str(len(Trip.objects.filter(location=self.location)) + 1).zfill(2)
+        self.code = self.code.replace('_xx', u'_{}'.format(next_id))
+        super(Trip, self).save(*args, **kwargs)
 
     @property
     def region(self):
