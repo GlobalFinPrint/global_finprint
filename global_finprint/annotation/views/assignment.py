6188aa2a0aa0fb282c1922dce0cdfbb95921eb9e
Robert Reis <RobertR@vulcan.com>
2016-04-05 12:29:01 -0700

    remove 2 more unused imports from assignment

--- global_finprint/annotation/views/assignment.py
+++ global_finprint/annotation/views/assignment.py
@@ -1,6 +1,4 @@
 from django.views.generic import View
-from django.contrib import messages
-from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import get_object_or_404, render_to_response
 from django.db.models import Count
 from django.http.response import JsonResponse
