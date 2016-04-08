9f66c17a1b4c44a65118997fbdafafdc44049c24
Robert Reis <RobertR@vulcan.com>
2016-04-08 14:42:40 -0700

    update user creation form

--- global_finprint/core/admin.py
+++ global_finprint/core/admin.py
@@ -1,24 +1,40 @@
+from django.forms import ChoiceField, RadioSelect
 from django.contrib.admin import site
 from django.contrib.auth import admin, forms, models
 from .models import Affiliation
 
 
 class UserCreationForm(forms.UserCreationForm):
+    role = ChoiceField(
+        choices=[('superuser', 'Superuser'), ('lead', 'Lead'), ('annotator', 'Annotator')],
+        widget=RadioSelect()
+    )
+
     class Meta:
         model = models.User
         fields = ('username', 'first_name', 'last_name', 'email')
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field in ['first_name', 'last_name', 'email']:
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+        for field in ['first_name', 'last_name', 'email', 'role']:
             self.fields[field].required = True
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=True)
+        role = self.cleaned_data.get('role')
+        user.groups = [2] if role == 'annotator' else [1, 2]
+        if role == 'superuser':
+            user.is_superuser = True
+            user.is_staff = True
+        user.save()
+        return user
 
 
 class UserAdmin(admin.UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'password1', 'password2', 'first_name', 'last_name', 'email'),
+            'fields': ('username', 'password1', 'password2', 'first_name', 'last_name', 'email', 'role'),
         }),
     )
     add_form = UserCreationForm
