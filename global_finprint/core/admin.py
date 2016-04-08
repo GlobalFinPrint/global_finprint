b465f74fa10bf2c8360acbcab3d4666a50cc2af4
Robert Reis <RobertR@vulcan.com>
2016-04-08 14:56:23 -0700

    refactor fpuser creation into admin form

--- global_finprint/core/admin.py
+++ global_finprint/core/admin.py
@@ -1,10 +1,14 @@
-from django.forms import ChoiceField, RadioSelect
+from django.forms import ChoiceField, ModelChoiceField, RadioSelect
 from django.contrib.admin import site
 from django.contrib.auth import admin, forms, models
-from .models import Affiliation
+from .models import Affiliation, FinprintUser
 
 
 class UserCreationForm(forms.UserCreationForm):
+    affiliation = ModelChoiceField(
+        queryset=Affiliation.objects.all(),
+        empty_label=None
+    )
     role = ChoiceField(
         choices=[('superuser', 'Superuser'), ('lead', 'Lead'), ('annotator', 'Annotator')],
         widget=RadioSelect()
@@ -16,9 +20,10 @@ class UserCreationForm(forms.UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super(UserCreationForm, self).__init__(*args, **kwargs)
-        for field in ['first_name', 'last_name', 'email', 'role']:
+        for field in ['first_name', 'last_name', 'email', 'affiliation', 'role']:
             self.fields[field].required = True
         self.fields['role'].initial = 'annotator'
+        self.fields['affiliation'].initial = 0
 
     def save(self, commit=True):
         user = super(UserCreationForm, self).save(commit=True)
@@ -28,6 +33,10 @@ class UserCreationForm(forms.UserCreationForm):
             user.is_superuser = True
             user.is_staff = True
         user.save()
+        FinprintUser(
+            user=user,
+            affiliation=self.cleaned_data.get('affiliation')
+        ).save()
         return user
 
 
@@ -35,7 +44,7 @@ class UserAdmin(admin.UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'password1', 'password2', 'first_name', 'last_name', 'email', 'role'),
+            'fields': ('username', 'password1', 'password2', 'first_name', 'last_name', 'email', 'affiliation', 'role'),
         }),
     )
     add_form = UserCreationForm
--- global_finprint/signals/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class SignalConfig(AppConfig):
-    name = 'global_finprint.signals'
-
-    def ready(self):
-        import global_finprint.signals.signals
