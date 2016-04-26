a397c2fe68f600011f3dc4c57da385ec77e17998
vkirkl <kirkl@vulcan.com>
2016-04-26 10:47:10 +0300

    change 'name' to 'group_name'

--- database/create_v_report_organism_list.sql
+++ database/create_v_report_organism_list.sql
@@ -8,7 +8,7 @@ CREATE OR REPLACE VIEW public.v_report_organism_list AS
     a.species,
     COALESCE(a.fishbase_key, 0)    AS fishbase_key,
     COALESCE(a.sealifebase_key, 0) AS sealifebase_key,
-    ag.name
+    ag.name as group_name
   FROM (annotation_animal a
     JOIN annotation_animalgroup ag ON ((ag.id = a.group_id)))
   ORDER BY ag.name, a.family, a.genus, a.species;
