a243ca4c30511f94daa39393b351a47611136b2c
vkirkl <kirkl@vulcan.com>
2016-05-16 09:58:41 -0700

    add bait to Admin

--- global_finprint/bruv/admin.py
+++ global_finprint/bruv/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from global_finprint.bruv import models
 
 
+admin.site.register(models.Bait)
 admin.site.register(models.FrameType)
 admin.site.register(models.Equipment)
 
