c60119aecf5f6c8e11147ad810806c4592419f80
Robert Reis <RobertR@vulcan.com>
2016-08-19 15:06:07 -0700

    [GLOB-215] remove step for lat/lng on set form

--- global_finprint/bruv/forms.py
+++ global_finprint/bruv/forms.py
@@ -61,8 +61,8 @@ class SetForm(forms.ModelForm):
         self.helper = FormHelper(self)
         self.helper.form_tag = False
         self.fields['code'].label += '**'
-        self.fields['latitude'].widget.attrs['step'] = 1.0
-        self.fields['longitude'].widget.attrs['step'] = 1.0
+        self.fields['latitude'].widget.attrs['step'] = 'any'
+        self.fields['longitude'].widget.attrs['step'] = 'any'
         help_text = '<small class="help-block">*Required Field &nbsp;&nbsp;&nbsp; **Note: If code is left blank, ' \
                     'it will be automatically generated.</small>'
         self.helper.layout.append(cfl.Div(cfl.HTML(help_text)))
