a3ee873bde137e63bd88a9435532bcad078b8722
Robert Reis <RobertR@vulcan.com>
2016-08-19 13:12:33 -0700

    [GLOB-212] adjust dissolved oxygen field max digits

--- global_finprint/bruv/models.py
+++ global_finprint/bruv/models.py
@@ -101,7 +101,7 @@ class EnvironmentMeasure(AuditableModel):
                                        max_digits=8, decimal_places=2,
                                        help_text='S/m')  # S/m .00
     dissolved_oxygen = models.DecimalField(null=True, blank=True,
-                                           max_digits=3, decimal_places=1)
+                                           max_digits=8, decimal_places=1)
     current_flow = models.DecimalField(null=True, blank=True,
                                        max_digits=5, decimal_places=2,
                                        help_text='m/s')  # m/s .00
