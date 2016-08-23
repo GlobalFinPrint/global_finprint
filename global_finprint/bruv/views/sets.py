f55faaeb543c7db6e94bb82f4cfdc48af7f05e73
vkirkl <kirkl@vulcan.com>
2016-08-23 14:09:46 -0700

    create env measures for set if missing and there are form values.  don't save if missing.

--- global_finprint/bruv/views/sets.py
+++ global_finprint/bruv/views/sets.py
@@ -10,7 +10,8 @@ from django.db import transaction
 
 from global_finprint.trip.models import Trip
 from global_finprint.bruv.models import Equipment
-from ..models import Set, HabitatSubstrate
+from global_finprint.annotation.models.video import Video
+from ..models import Set, HabitatSubstrate, EnvironmentMeasure
 from ..forms import SetForm, EnvironmentMeasureForm, \
     SetSearchForm, SetLevelCommentsForm, SetLevelDataForm
 from ...annotation.forms import VideoForm
@@ -247,12 +248,31 @@ class SetListView(UserAllowedMixin, View):
                 for k, v in set_form.cleaned_data.items():
                     if k not in ('reef', 'habitat'):
                         setattr(edited_set, k, v)
-                for k, v in drop_form.cleaned_data.items():
-                    setattr(edited_set.drop_measure, k, v)
-                for k, v in haul_form.cleaned_data.items():
-                    setattr(edited_set.haul_measure, k, v)
-                for k, v in video_form.cleaned_data.items():
-                    setattr(edited_set.video, k, v)
+
+                # check for children that might be missing but have data in their forms:
+                if not edited_set.drop_measure and \
+                    any(x is not None for x in list(drop_form.cleaned_data.values())):
+                    # note: the dissolved_oxygen_measure shows up as a value and should probably be filtered out ...
+                    # otherwise measure is always created.
+                    edited_set.drop_measure = EnvironmentMeasure.objects.create()
+                if not edited_set.haul_measure and \
+                    any(x is not None for x in list(haul_form.cleaned_data.values())):
+                    # note: again, the dissolved_oxygen_measure shows up as a value and should probably be filtered out?
+                    edited_set.haul_measure = EnvironmentMeasure.objects.create()
+                if not edited_set.video and \
+                    any(x is not None for x in list(video_form.cleaned_data.values())):
+                    edited_set.video = Video.objects.create()
+
+                # guard against possibly missing drop_measure, haul_measure or video:
+                if edited_set.drop_measure:
+                    for k, v in drop_form.cleaned_data.items():
+                        setattr(edited_set.drop_measure, k, v)
+                if edited_set.haul_measure:
+                    for k, v in haul_form.cleaned_data.items():
+                        setattr(edited_set.haul_measure, k, v)
+                if edited_set.video:
+                    for k, v in video_form.cleaned_data.items():
+                        setattr(edited_set.video, k, v)
                 for k, v in set_level_data_form.cleaned_data.items():
                     if k not in ('bruv_image_file', 'splendor_image_file'):
                         setattr(edited_set, k, v)
@@ -260,10 +280,14 @@ class SetListView(UserAllowedMixin, View):
                     setattr(edited_set, k, v)
 
                 edited_set.save()
-                edited_set.bait.save()
-                edited_set.drop_measure.save()
-                edited_set.haul_measure.save()
-                edited_set.video.save()
+                if edited_set.bait:
+                    edited_set.bait.save()
+                if edited_set.drop_measure:
+                    edited_set.drop_measure.save()
+                if edited_set.haul_measure:
+                    edited_set.haul_measure.save()
+                if edited_set.video:
+                    edited_set.video.save()
 
                 # upload and save image urls
                 self._process_habitat_images(edited_set, request)
