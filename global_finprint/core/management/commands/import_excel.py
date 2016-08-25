ab90437ab72196d6d26e5961d7181541d31d6a13
Tyler Sellon <c-tylers@vulcan.com>
2016-08-25 11:19:14 -0700

    Factor common excel functions into a separate module.

--- global_finprint/core/management/commands/import_excel.py
+++ global_finprint/core/management/commands/import_excel.py
@@ -11,19 +11,18 @@ https://www.dropbox.com/s/5yy0bb4mxbm0mdj/data_collection_standards.xlsx?dl=0
 """
 import openpyxl
 import os
-from datetime import datetime
 import logging
 import traceback
 
 from django.core.management.base import BaseCommand, CommandError
 
 import global_finprint.core.management.commands.import_common as ic
+import global_finprint.core.management.commands.excel_common as ec
 
 logger = logging.getLogger('scripts')
 
 def import_file(in_file):
-    'Trip, Set, Environment, Observation'
-    wb = openpyxl.load_workbook(in_file)
+    wb = ec.open_workbook(in_file)
 
     import_trip_data(wb['Trip'])
     import_set_data(wb['Set'])
@@ -31,15 +30,15 @@ def import_file(in_file):
     import_observation_data(wb['Observation'])
 
 def import_trip_data(sheet):
-    headers = get_header_map(sheet.rows[0])
-    get_cell = get_cell_by_name_extractor(headers)
+    headers = ec.get_header_map(sheet.rows[0])
+    get_cell = ec.get_cell_by_name_extractor(headers)
     for idx, row in enumerate(sheet.rows[1:], start=2):
         try:
             trip_code = get_cell(row, 'code').value
             if trip_code:
                 location_name = get_cell(row, 'location').value
-                start_date = get_date_from_cell(get_cell(row, 'start_date'))
-                end_date = get_date_from_cell(get_cell(row, 'end_date'))
+                start_date = ec.get_date_from_cell(get_cell(row, 'start_date'))
+                end_date = ec.get_date_from_cell(get_cell(row, 'end_date'))
                 investigator = get_cell(row, 'investigator').value
                 collaborator = get_cell(row, 'collaborator').value
                 boat = get_cell(row, 'boat').value
@@ -58,27 +57,27 @@ def import_trip_data(sheet):
             logger.error(traceback.format_exc())
 
 def import_set_data(sheet):
-    headers = get_header_map(sheet.rows[0])
-    get_cell = get_cell_by_name_extractor(headers)
+    headers = ec.get_header_map(sheet.rows[0])
+    get_cell = ec.get_cell_by_name_extractor(headers)
     for idx, row in enumerate(sheet.rows[1:], start=2):
         try:
             set_code = get_cell(row, 'set_code').value
             if set_code:
                 trip_code = get_cell(row, 'trip_code').value
-                set_date = get_date_from_cell(get_cell(row, 'date'))
+                set_date = ec.get_date_from_cell(get_cell(row, 'date'))
                 latitude = get_cell(row, 'latitude').value
                 longitude = get_cell(row, 'longitude').value
                 try:
                     depth_cell = get_cell(row, 'depth')
-                    depth = get_float_from_cell(depth_cell)
+                    depth = ec.get_float_from_cell(depth_cell)
                 except ValueError:
                     logger.error('Bad depth value "%s"', depth_cell.value)
                     logger.error('Set "%s" not created', set_code)
                     continue
-                drop_time = get_time_from_cell(get_cell(row, 'drop_time'), format_str='%H:%M')
-                haul_time = get_time_from_cell(get_cell(row, 'haul_time'), format_str='%H:%M')
-                site_name = get_cell_value(get_cell(row, 'site'))
-                reef_name = get_cell_value(get_cell(row, 'reef'))
+                drop_time = ec.get_time_from_cell(get_cell(row, 'drop_time'), format_str='%H:%M')
+                haul_time = ec.get_time_from_cell(get_cell(row, 'haul_time'), format_str='%H:%M')
+                site_name = ec.get_cell_value(get_cell(row, 'site'))
+                reef_name = ec.get_cell_value(get_cell(row, 'reef'))
                 habitat_type = get_cell(row, 'habitat').value
                 equipment_str = get_cell(row, 'equipment').value
                 bait_str = get_cell(row, 'bait').value
@@ -119,25 +118,25 @@ def import_set_data(sheet):
             
 
 def import_environment_data(sheet):
-    headers = get_header_map(sheet.rows[0])
-    get_cell = get_cell_by_name_extractor(headers)
+    headers = ec.get_header_map(sheet.rows[0])
+    get_cell = ec.get_cell_by_name_extractor(headers)
     for idx, row in enumerate(sheet.rows[1:], start=2):
         try:
             trip_code = get_cell(row, 'trip_code').value
             if trip_code:
                 set_code = get_cell(row, 'set_code').value
-                reading_date = get_date_from_cell(get_cell(row, 'date'))
+                reading_date = ec.get_date_from_cell(get_cell(row, 'date'))
                 drop_haul = get_cell(row, 'drop_haul').value
-                temp = get_float_from_cell(get_cell(row, 'temp'))
-                salinity = get_float_from_cell(get_cell(row, 'salinity'))
-                conductivity = get_float_from_cell(get_cell(row, 'conductivity'))
-                dissolved_oxygen = get_float_from_cell(get_cell(row, 'dissolved_oxygen'))
+                temp = ec.get_float_from_cell(get_cell(row, 'temp'))
+                salinity = ec.get_float_from_cell(get_cell(row, 'salinity'))
+                conductivity = ec.get_float_from_cell(get_cell(row, 'conductivity'))
+                dissolved_oxygen = ec.get_float_from_cell(get_cell(row, 'dissolved_oxygen'))
                 current_flow = None #get_float_from_cell(get_cell(row, 'current_flow'))
                 current_direction = get_cell(row, 'current_direction').value
-                tide_state = get_cell_value(get_cell(row, 'tide_state'))
-                wind_speed = get_float_from_cell(get_cell(row, 'wind_speed'))
+                tide_state = ec.get_cell_value(get_cell(row, 'tide_state'))
+                wind_speed = ec.get_float_from_cell(get_cell(row, 'wind_speed'))
                 wind_direction = get_cell(row, 'wind_direction').value
-                cloud_cover = get_float_from_cell(get_cell(row, 'cloud_cover'))
+                cloud_cover = ec.get_float_from_cell(get_cell(row, 'cloud_cover'))
                 surface_chop = get_cell(row, 'surface_chop').value
 
                 ic.import_environment_measure(
@@ -163,13 +162,13 @@ def import_environment_data(sheet):
 
 
 def import_observation_data(sheet):
-    headers = get_header_map(sheet.rows[0])
-    get_cell = get_cell_by_name_extractor(headers)
+    headers = ec.get_header_map(sheet.rows[0])
+    get_cell = ec.get_cell_by_name_extractor(headers)
     for row in sheet.rows[1:]:
         trip_code = get_cell(row, 'trip_code').value
         if trip_code:
             set_code = get_cell(row, 'set_code').value
-            obvs_date = get_date_from_cell(get_cell(row, 'date'))
+            obvs_date = ec.get_date_from_cell(get_cell(row, 'date'))
             obvs_time = get_cell(row, 'time').value
             duration = get_cell(row, 'duration').value
             family = get_cell(row, 'family').value
@@ -181,7 +180,7 @@ def import_observation_data(sheet):
             length = get_cell(row, 'length').value
             comment = get_cell(row, 'comment').value
             annotator = get_cell(row, 'annotator').value
-            annotation_date = get_date_from_cell(get_cell(row, 'date'))
+            annotation_date = ec.get_date_from_cell(get_cell(row, 'date'))
 
             ic.import_observation(
                 trip_code,
@@ -200,40 +199,6 @@ def import_observation_data(sheet):
                 annotator,
                 annotation_date
             )
-
-def get_cell_by_name_extractor(headers):
-    extractor_func = lambda row, column_name: row[headers[column_name]]
-    return extractor_func
-
-def get_cell_value(cell):
-    return cell.value.strip()
-
-def get_header_map(header_row):
-    result = {}
-    for idx, header in enumerate(header_row):
-        if header.value:
-            result[header.value] = idx
-    return result
-
-def get_float_from_cell(cell):
-    result = None
-    if isinstance(cell.value, str):
-        result = float(cell.value)
-    else:
-        result = cell.value
-    return result
-
-def get_date_from_cell(cell):
-    result = cell.value
-    if isinstance(result, str):
-        result = datetime.strptime(cell.value, '%d/%m/%Y')
-    return result
-    
-def get_time_from_cell(cell, format_str='%H:%M:%S %p'):
-    result = cell.value
-    if isinstance(result, str):
-        result = datetime.strptime(cell.value, '%H:%M:%S %p').time()
-    return result
 
 class Command(BaseCommand):
     help = 'Imports observation data from excel format. Usage: python manage.py import_excel <excel_file>'
