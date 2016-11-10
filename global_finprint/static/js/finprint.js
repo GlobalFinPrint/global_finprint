a7540accd849eb710582762cf9ea7398ed942b8e
Robert Reis <RobertR@vulcan.com>
2016-11-10 14:26:57 -0800

    include colorbrewer on all pages

--- global_finprint/static/js/finprint.js
+++ global_finprint/static/js/finprint.js
@@ -21,6 +21,7 @@ var finprint = finprint || {};  //namespace if necessary...
         initSubstrateWidget();
         initCheckbuttons();
         initColoredRows();
+        initDisableOnSubmit();
     });
 
     function getCSRF() {
@@ -653,5 +654,11 @@ var finprint = finprint || {};  //namespace if necessary...
             }
             $(cell).attr('data-pall-index', diffDict[diffKey]);
         });
+    }
+
+    function initDisableOnSubmit() {
+        // $('input[type="submit"]').click(function() {
+        //     $('input[type="submit"]').attr('disabled', 'disabled');
+        // });
     }
 })(jQuery);
