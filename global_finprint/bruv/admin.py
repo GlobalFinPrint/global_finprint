f1085beabb3c8b4ca080f8ea6a5439ba07690816
Robert Reis <RobertR@vulcan.com>
2016-11-30 16:10:27 -0800

    [GLOB-431] remove add and change inline actions for tag parent, benthic category parent, and set tag parent

--- global_finprint/bruv/admin.py
+++ global_finprint/bruv/admin.py
@@ -11,10 +11,22 @@ admin.site.register(models.Equipment)
 class SetTagAdmin(MPTTModelAdmin):
     fields = ('parent', 'name', 'description', 'active')
 
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(SetTagAdmin, self).get_form(request, obj, **kwargs)
+        form.base_fields['parent'].widget.can_add_related = False
+        form.base_fields['parent'].widget.can_change_related = False
+        return form
+
 admin.site.register(models.SetTag, SetTagAdmin)
 
 
 class BenthicCategoryAdmin(MPTTModelAdmin):
     fields = ('parent', 'name', 'description', 'active')
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(BenthicCategoryAdmin, self).get_form(request, obj, **kwargs)
+        form.base_fields['parent'].widget.can_add_related = False
+        form.base_fields['parent'].widget.can_change_related = False
+        return form
 
 admin.site.register(models.BenthicCategory, BenthicCategoryAdmin)
