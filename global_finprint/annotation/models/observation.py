370dff9142aa18c9102da5affa3c0b948bbf9d48
vkirkl <kirkl@vulcan.com>
2017-04-04 15:53:03 -0700

    fix S3 bucket name regression.

--- global_finprint/annotation/models/observation.py
+++ global_finprint/annotation/models/observation.py
@@ -283,7 +283,7 @@ class AbstractEvent(AuditableModel):
 
         try:
             conn = S3Connection(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
-            bucket = conn.get_bucket(settings.AWS_S3_FRAME_CAPTURE_BUCKET)
+            bucket = conn.get_bucket(settings.FRAME_CAPTURE_BUCKET)
             key = bucket.get_key(self.filename())
             return key.generate_url(expires_in=300, query_auth=False) if key else None
         except S3ResponseError:
