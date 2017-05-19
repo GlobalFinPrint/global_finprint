56d838700c0e3e6c06c7238cd4b1b1fb0e8e0a51
vkirkl <kirkl@vulcan.com>
2017-05-19 14:33:40 -0700

    refactor master record urls.

--- global_finprint/annotation/urls/assignment.py
+++ global_finprint/annotation/urls/assignment.py
@@ -1,35 +1,22 @@
-from django.conf.urls import url
-from django.views.decorators.csrf import csrf_exempt
+from django.conf.urls import url, include
+
 from global_finprint.annotation.views.assignment import VideoAutoAssignView, AssignmentManageView, \
     AssignmentListView, AssignmentListTbodyView, AssignmentModalBodyView, UnassignModalBodyView
-from global_finprint.annotation.views.compare import AssignmentCompareView, AssignmentDetailView, \
-    GetMasterView, MasterReviewView, MasterSetCompleted, MasterSetDeprecated, MasterManageView
-from global_finprint.annotation.views.observations import MasterObservationEditData, \
-    MasterObservationSaveData, EditMeasurablesInline
+from global_finprint.annotation.views.compare import AssignmentCompareView, AssignmentDetailView
+
 
 urlpatterns = [
     url(r"^$", AssignmentListView.as_view(), name='assignment_list'),
     url(r"^search$", AssignmentListTbodyView.as_view(), name='assignment_search'),
     url(r"^manage/(?P<assignment_id>\d+)$", AssignmentManageView.as_view(), name='assignment_manage'),
+
     url(r"^modal/(?P<set_id>\d+)$", AssignmentModalBodyView.as_view(), name='assignment_modal'),
     url(r"^unassign_modal/(?P<assignment_id>\d+)$", UnassignModalBodyView.as_view(), name='unassign_modal'),
     url(r"^auto$", VideoAutoAssignView.as_view(), name='auto_assign'),
-    url(r"^compare/(?P<set_id>\d+)$", AssignmentCompareView.as_view(), name='assignment_compare'),
-    url(r"^review/(?P<master_id>\d+)$", MasterReviewView.as_view(), name='master_review'),
+
     url(r"^detail/(?P<assignment_id>\d+)$", AssignmentDetailView.as_view(), name='get_assignment_detail'),
-    url(r"^master/(?P<set_id>\d+)$", csrf_exempt(GetMasterView.as_view()), name='get_master'),
-    url(r"^master/(?P<set_id>\d+)/edit_data/(?P<evt_id>\d+)$", csrf_exempt(MasterObservationEditData.as_view()),
-        name='master_edit_obs'),
-    url(r"^master/(?P<set_id>\d+)/save_data/(?P<evt_id>\d+)$", csrf_exempt(MasterObservationSaveData.as_view()),
-        name='master_save_obs'),
 
-    url(r"^master/(?P<master_id>\d+)/complete$", csrf_exempt(MasterSetCompleted.as_view()),
-        name='master_set_completed'),
-    url(r"^master/(?P<master_id>\d+)/deprecate$", csrf_exempt(MasterSetDeprecated.as_view()),
-        name='master_set_deprecated'),
-    url(r"^master/manage/(?P<master_id>\d+)$", csrf_exempt(MasterManageView.as_view()),
-        name='master_set_deprecated'),
+    url(r"^compare/(?P<set_id>\d+)$", AssignmentCompareView.as_view(), name='assignment_compare'),
 
-    url(r"^master/edit_measurables/(?P<evt_id>\d+)$", csrf_exempt(EditMeasurablesInline.as_view()),
-        name='edit_measurables_inline')
+    url(r"^master/", include('global_finprint.annotation.urls.master')),
 ]
