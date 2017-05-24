44bcd518e638b046974277cd97832e1efd3f1422
vkirkl <kirkl@vulcan.com>
2017-05-24 09:14:21 -0700

    exclude all but 'complete' assignments on compare.

--- global_finprint/annotation/views/compare.py
+++ global_finprint/annotation/views/compare.py
@@ -17,6 +17,7 @@ class AssignmentCompareView(UserAllowedMixin, View):
     def get(self, request, set_id):
         set = get_object_or_404(Set, pk=set_id)
         project = get_object_or_404(Project, pk=request.GET.get('project', 1))
+        # default to master status 'in progress':
         master_status = get_object_or_404(MasterRecordState, pk=1)
         master, created = MasterRecord.objects.get_or_create(
             set=set, project=project, defaults={'status': master_status})
@@ -26,6 +27,7 @@ class AssignmentCompareView(UserAllowedMixin, View):
             'video_length': set.video.length(),
             'master': master,
             'project': project,
+            # exclude assignments that are 'rejected' or 'disabled':
             'assignment_set': set.video.assignment_set.exclude(status__in=[5, 6]).filter(project=project)
         })
         return render(request, self.template_name, context=context)
