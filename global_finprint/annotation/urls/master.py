6055722253667673a3ae3f648c485ea7c2bb8d25
vkirkl <kirkl@vulcan.com>
2017-05-31 14:02:20 -0700

    Fix master save, edit and measurables urls.

--- global_finprint/annotation/urls/master.py
+++ global_finprint/annotation/urls/master.py
@@ -12,11 +12,11 @@ urlpatterns = [
     url(r"^review/(?P<master_id>\d+)$", MasterObservationListView.as_view(), name='master_review'),
     url(r"^manage/(?P<master_id>\d+)$", ManageMasterView.as_view(), name='master_manage'),
 
-    url(r"^master/(?P<set_id>\d+)/edit_data/(?P<evt_id>\d+)$", csrf_exempt(MasterObservationEditData.as_view()),
+    url(r"^(?P<set_id>\d+)/edit_data/(?P<evt_id>\d+)$", csrf_exempt(MasterObservationEditData.as_view()),
         name='master_edit_obs'),
-    url(r"^master/(?P<set_id>\d+)/save_data/(?P<evt_id>\d+)$", csrf_exempt(MasterObservationSaveData.as_view()),
+    url(r"^(?P<set_id>\d+)/save_data/(?P<evt_id>\d+)$", csrf_exempt(MasterObservationSaveData.as_view()),
         name='master_save_obs'),
 
-    url(r"^master/edit_measurables/(?P<evt_id>\d+)$", csrf_exempt(EditMeasurablesInline.as_view()),
+    url(r"^edit_measurables/(?P<evt_id>\d+)$", csrf_exempt(EditMeasurablesInline.as_view()),
         name='edit_measurables_inline')
 ]
