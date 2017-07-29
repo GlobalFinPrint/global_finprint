ce5b6f6f3f6622cfcd60dfee00238513dce2fd53
vkirkl <kirkl@vulcan.com>
2017-07-29 13:25:20 -0700

    django models and endpoints for obs and set summaries.

--- global_finprint/report/urls.py
+++ global_finprint/report/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url, include
-from .views import CustomReportListView, CustomReportView, CustomReportFileView, LeaderboardView, StatusMapView
+from .views import CustomReportListView, CustomReportView, \
+    CustomReportFileView, LeaderboardView, StatusMapView, \
+    HabitatSummaryView, ObservationSummaryView, SetSummaryView
 
 
 urlpatterns = [
@@ -11,6 +13,10 @@ urlpatterns = [
     url(r"custom/(?P<report>\w+)$", CustomReportView.as_view(), name="report_custom"),
     # todo:  generify the format from only .csv?
     url(r"custom/(?P<report>\w+).(?P<format>\w+)$", CustomReportFileView.as_view(), name="report_custom_csv"),
-    url(r"$", CustomReportListView.as_view(), name="report_home"),
 
+    url(r"habitat/$", HabitatSummaryView.as_view(), name="habitats"),
+    url(r"observation/summary/(?P<region>[\w ]+)$", ObservationSummaryView.as_view(), name="observation_summary"),
+    url(r"set/summary/(?P<region>[\w ]+)$", SetSummaryView.as_view(), name="set_summary"),
+
+    url(r"$", CustomReportListView.as_view(), name="report_home"),
 ]
