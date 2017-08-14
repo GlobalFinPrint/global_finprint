98493cf0734160e2613c9c3dc6f7ff9dfd03c7ac
vkirkl <kirkl@vulcan.com>
2017-08-14 14:11:58 -0700

    GA view comments

--- global_finprint/api/urls/ga.py
+++ global_finprint/api/urls/ga.py
@@ -1,46 +1,5 @@
 from django.conf.urls import url
-from django.views.decorators.csrf import csrf_exempt
-from global_finprint.api import views
 
 urlpatterns = [
-    ######
-    # Global Archive
-    # Sampling: Transect dimension (length m x width m)
-    # n/a
 
-    # Sampling: Deployment duration (min)
-    # diff
-
-    # Sampling: Bait type
-
-    # Sampling: Bait quantity (g)
-    # assumed to be 1000g
-
-    # Sampling: Bait deployment
-
-    # Sampling: Bait consistency
-
-    # Sampling: Minimum separation distance of samples (m)
-
-    # Sampling: Camera type
-    # Sampling: Camera configuration
-    # Sampling: Camera separation (mm)
-    # Sampling: Camera incidence angle (degrees)
-    # Sampling: Camera height above benthos (mm)
-    # Sampling: Bait arm length (m)
-    # Sampling: Frame type
-    # Sampling: Artificial lighting
-    # Annotation: Upload type
-    # Annotation: Annotation tool/software
-    # Annotation: Taxa that have been ID'd
-    # Annotation: Taxonomic resolution
-    # Annotation: Observer/Anotator experience (>years)
-    # Annotation: Observation/Annotation checked by expert
-    # Annotation: Lengths measured for
-    # Annotation: Type of Length measure
-    # Annotation: Maximum range annotations were made (m)
-    # Annotation: Area over which annotations were made (m2)
-    # Annotation: Habitat information
-    # Annotation: Habitat data
-    # Annotation: .EMObs uploaded as .zip
 ]
---
+++ global_finprint/api/views/ga.py
@@ -0,0 +1,59 @@
+# Global Archive campaign data
+    #
+# Sampling: Transect dimension (length m x width m)
+    # n/a
+# Sampling: Deployment duration (min)
+    # haul_time - drop_time
+# Sampling: Bait type
+    # Bait.description
+# Sampling: Bait quantity (g)
+    # assumed to be 1000g
+# Sampling: Bait deployment
+    # Equipment.bait_container
+# Sampling: Bait consistency
+    # Bait.type
+# Sampling: Minimum separation distance of samples (m)
+    # n/a
+# Sampling: Camera type
+    # Equipment.camera_type
+# Sampling: Camera configuration
+    # n/a
+# Sampling: Camera separation (mm)
+    # n/a
+# Sampling: Camera incidence angle (degrees)
+    # n/a
+# Sampling: Camera height above benthos (mm)
+    # Equipment.camera_height
+# Sampling: Bait arm length (m)
+    # Equipment.arm_length
+# Sampling: Frame type
+    # FrameType
+# Sampling: Artificial lighting
+    # n/a
+
+# Annotation: Upload type
+    # n/a
+# Annotation: Annotation tool/software
+    # Finprint Annotator
+# Annotation: Taxa that have been ID'd
+    #
+# Annotation: Taxonomic resolution
+    #
+# Annotation: Observer/Anotator experience (>years)
+    # n/a
+# Annotation: Observation/Annotation checked by expert
+    # n/a ... assumed for master data
+# Annotation: Lengths measured for
+    # n/a
+# Annotation: Type of Length measure
+    # n/a
+# Annotation: Maximum range annotations were made (m)
+    # n/a
+# Annotation: Area over which annotations were made (m2)
+    # n/a
+# Annotation: Habitat information
+    # n/a
+# Annotation: Habitat data
+    # n/a
+# Annotation: .EMObs uploaded as .zip
+    # n/a
