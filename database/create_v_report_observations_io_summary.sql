2e673f4a189d8a71966366b2a5e51245e8e7c5b2
vkirkl <kirkl@vulcan.com>
2017-09-12 14:07:07 -0700

    updates to obs summaries for maxn measurable.

--- database/create_v_report_observations_io_summary.sql
+++ database/create_v_report_observations_io_summary.sql
@@ -1,147 +1,32 @@
 CREATE OR REPLACE VIEW public.v_report_observations_io_summary AS
-WITH zero_times AS
-(
-    SELECT
-      set_id,
-      video_id,
-      max(
-          CASE
-          WHEN zero_time_tagged = 0
-            THEN 0
-          ELSE event_time
-          END) AS zero_time
-    FROM event_attribute_summary
-    GROUP BY set_id,
-      video_id
-),
-    -- maxn by event count by set
-    event_count_maxn AS
-(
-    SELECT
-      count(evt.id) AS event_count,
-      evt.event_time,
-      aobs.animal_id,
-
-      s.id as set_id,
-      evt.observation_id
-    FROM annotation_event evt
-      INNER JOIN annotation_animalobservation aobs ON aobs.observation_id = evt.observation_id
-      INNER JOIN annotation_observation obs ON obs.id = evt.observation_id
-      INNER JOIN annotation_assignment assig ON assig.id = obs.assignment_id
-      INNER JOIN bruv_set s ON s.video_id = assig.video_id
-    GROUP BY
-      evt.event_time,
-      evt.observation_id,
-      aobs.animal_id,
-      s.id
-),
-    -- maxn from attribute and note by set
-    attribute_maxn AS
-(
-    SELECT
-      max(evtsum.numeric_value_from_event_note) :: BIGINT    AS note_count,
-      evtsum.event_time,
-      aobs.animal_id,
-
-      evtsum.set_id
-    FROM event_attribute_summary evtsum
-        INNER JOIN annotation_animalobservation aobs ON aobs.observation_id = evtsum.observation_id
-    WHERE evtsum.numeric_value_from_event_note IS NOT NULL
-          AND evtsum.max_n_tagged = 1
-    GROUP BY
-      evtsum.event_time,
-      aobs.animal_id,
-      evtsum.set_id
-),
-    -- merged results
-    full_maxn as
-(
-  SELECT
-    case when not(event_count is null) and not(note_count is null)
-    then
-      case
-        when event_count > note_count
-        then event_count
-        else note_count
-      end
-    else coalesce(ecm.event_count, atm.note_count)
-    end AS maxn,
-    coalesce(ecm.event_time, atm.event_time) as event_time,
-    coalesce(ecm.animal_id, atm.animal_id) as animal_id,
-    coalesce(ecm.set_id, atm.set_id) as set_id
-  FROM event_count_maxn ecm
-  FULL OUTER JOIN attribute_maxn atm ON (
-      ecm.event_time = atm.event_time
-      and ecm.animal_id = atm.animal_id
-      and ecm.set_id = atm.set_id
-    )
-),
-  -- reduced to only "max" maxn as some record increases
-  reduced_maxn as
-(
-  SELECT
-    max(maxn) as maxn,
-    animal_id,
-    set_id
-  FROM full_maxn
-  GROUP BY animal_id,
-    set_id
-)
--- earliest occurance of maxn + a bunch of other useful data
-SELECT
-  t.code as trip_code,
-  s.code as set_code,
-  t.code || '_' || s.code                        AS full_code,
-
-  hab.region_name                                 AS region_name,
-  hab.location_name                               AS location_name,
-  hab.site_name                                   AS site_name,
-  hab.reef_name                                   AS reef_name,
-  hab.reef_habitat_name                           AS reef_habitat_name,
-
-  ani.id as animal_id,
-  ani.family,
-  ani.genus,
-  ani.species,
-
-  reduced_maxn.maxn,
-  public.text_time(min(full_maxn.event_time))               AS event_time_minutes_raw,
-  public.text_time(min(full_maxn.event_time) - min(z.zero_time)) AS event_time_minutes,
-
-  t.id as trip_id,
-  s.id                                            AS set_id,
-  s.video_id
-
-FROM reduced_maxn
-  inner join full_maxn on (
-    full_maxn.maxn = reduced_maxn.maxn
-    and full_maxn.animal_id = reduced_maxn.animal_id
-    and full_maxn.set_id = reduced_maxn.set_id
-    )
-  INNER JOIN annotation_animal ani ON ani.id = reduced_maxn.animal_id
-  INNER JOIN bruv_set s ON s.id = reduced_maxn.set_id
-  INNER JOIN trip_trip t ON t.id = s.trip_id
-
-  INNER JOIN habitat_summary hab ON hab.reef_habitat_id = s.reef_habitat_id
-  INNER JOIN zero_times z ON z.video_id = s.video_id
-where
-  hab.region_name = 'Indian Ocean'
-group by
-  t.code,
-  s.code,
+select
+  summary_id,
+  trip_code,
+  set_code,
   full_code,
 
-  reduced_maxn.maxn,
-  hab.region_name,
-  hab.location_name,
-  hab.site_name,
-  hab.reef_name,
-  hab.reef_habitat_name,
+  region_name,
+  location_name,
+  site_name,
+  reef_name,
+  reef_habitat_name,
 
-  ani.id,
-  ani.family,
-  ani.genus,
-  ani.species,
-  t.id,
-  s.id,
-  s.video_id;
+  animal_id,
+  family,
+  genus,
+  species,
+
+  maxn,
+  event_time_minutes_raw,
+  event_time_minutes,
+
+  trip_id,
+  set_id,
+  video_id
+from public.observation_summary
+where
+  lower(region_name) = lower('indian ocean')
+order by
+  location_name,
+  full_code,
+  event_time_minutes;
--- database/create_v_report_sets_without_video.sql
+++ database/create_v_report_sets_without_video.sql
@@ -1,21 +1,32 @@
 -- noinspection SqlNoDataSourceInspectionForFile
 CREATE OR REPLACE VIEW public.v_report_sets_without_video AS
-  WITH video_count AS (
-      SELECT
-        tt.code || '_' || bs.code AS code,
-        bs.create_datetime        AS set_creation_date,
-        count(avf.*)              AS video_count
-      FROM bruv_set bs
-        JOIN trip_trip tt ON (bs.trip_id = tt.id)
-        JOIN trip_source ts ON (tt.source_id = ts.id)
-        JOIN annotation_video av ON (bs.video_id = av.id)
-        LEFT JOIN annotation_videofile avf ON (av.id = avf.video_id)
-      WHERE
-        ts.legacy = FALSE
-      GROUP BY ts.id, tt.id, bs.id, av.id
-  )
-  SELECT
-    code,
-    set_creation_date
-  FROM video_count
-  WHERE video_count = 0;
+WITH video_count AS (
+    SELECT
+      bs.id,
+      bs.reef_habitat_id,
+      tt.code || '_' || bs.code AS code,
+      bs.create_datetime        AS set_creation_date,
+      count(avf.*)              AS video_count
+    FROM bruv_set bs
+      JOIN trip_trip tt ON (bs.trip_id = tt.id)
+      JOIN trip_source ts ON (tt.source_id = ts.id)
+      JOIN annotation_video av ON (bs.video_id = av.id)
+      LEFT JOIN annotation_videofile avf ON (av.id = avf.video_id)
+    WHERE
+      ts.legacy = FALSE
+    GROUP BY ts.id, tt.id, bs.id, av.id
+)
+SELECT
+  rg.name              AS region_name,
+  l.name               AS location_name,
+  coalesce(s.name, '') AS site_name,
+  coalesce(r.name, '') AS reef_name,
+  vc.code,
+  set_creation_date
+FROM habitat_region rg
+  JOIN habitat_location l ON l.region_id = rg.id
+  JOIN habitat_site s ON s.location_id = l.id
+  JOIN habitat_reef r ON r.site_id = s.id
+  JOIN habitat_reefhabitat rh ON rh.reef_id = r.id
+  JOIN video_count vc ON vc.reef_habitat_id = rh.id
+WHERE video_count = 0;
