43c8380bf0ef88ccc354ae0dbdc9aa0bb9e79752
vkirkl <kirkl@vulcan.com>
2017-09-14 14:30:17 -0700

    fresh help docs (but still need updates!)

--- global_finprint/help/views.py
+++ global_finprint/help/views.py
@@ -1,16 +1,18 @@
-from django.views.generic import View
-from django.shortcuts import render
+from django.views.generic import RedirectView
+from django.contrib.staticfiles.templatetags.staticfiles import static
 
 
-class WebHelpView(View):
-    template = 'pages/help/web_help.html'
+class WebHelpView(RedirectView):
+    permanent = True
+    query_string = True
 
-    def get(self, request):
-        return render(request, self.template)
+    def get_redirect_url(self, *args, **kwargs):
+        return static('pdf/Finprint Website User Guide.pdf')
 
 
-class ClientHelpView(View):
-    template = 'pages/help/client_help.html'
+class ClientHelpView(RedirectView):
+    permanent = True
+    query_string = True
 
-    def get(self, request):
-        return render(request, self.template)
+    def get_redirect_url(self, *args, **kwargs):
+        return static('pdf/Finprint Annotator User Guide.pdf')
