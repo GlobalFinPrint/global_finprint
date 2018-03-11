c2176a4d50a636c728b21a909e82b26792a6cb4e
Taylor <tmgorham@gmail.com>
2018-03-10 21:35:00 -0600

    improve reef summary

--- database/create_v_report_reef_summary.sql
+++ database/create_v_report_reef_summary.sql
@@ -19,49 +19,22 @@ CREATE OR REPLACE VIEW public.v_report_reef_summary AS
         assigst.id,
         assigst.name
   ),
-      set_summary AS (
+    set_summary AS (
         SELECT
           s.trip_id,
           s.id AS set_id,
           s.reef_habitat_id,
-          CASE
-          WHEN sum(COALESCE(assstat.status_count, 0)) >= 1
-            THEN 1
-          ELSE 0
-          END  AS has_assignments,
+          habitat_reefhabitat.reef_id,
           CASE
           WHEN (
                  SELECT sum(assstatcom.status_count)
                  FROM assignment_status_summary assstatcom
-                 WHERE assstatcom.assignment_status_id IN (3, 4, 5, 6)
+                 WHERE assstatcom.assignment_status_id IN (3, 4)
                        AND assstatcom.set_id = S.id
-               ) >= 2
+               ) >= 1
             THEN 1
           ELSE 0
-          END  AS has_two_complete_assignments,
-          CASE
-          WHEN (
-                 SELECT sum(assstatcom.status_count)
-                 FROM assignment_status_summary assstatcom
-                 WHERE assstatcom.assignment_status_id IN (4, 5, 6)
-                       AND assstatcom.set_id = S.id
-               ) >= 2
-            THEN 1
-          ELSE 0
-          END  AS has_two_reviewed_assignments,
-          CASE
-          WHEN
-            (sum(COALESCE(assstat.status_count, 0)) = 1)
-              AND
-              (
-                 SELECT sum(assstatcom.status_count)
-                 FROM assignment_status_summary assstatcom
-                 WHERE assstatcom.assignment_status_id IN (4, 5, 6)
-                       AND assstatcom.set_id = S.id
-               ) = 1
-            THEN 1
-          ELSE 0
-          END  AS one_assignment_one_reviewed,
+          END  AS min_1_complete_annotation,
           CASE
           WHEN mas.id IS NOT NULL
             THEN 1
@@ -72,21 +45,13 @@ CREATE OR REPLACE VIEW public.v_report_reef_summary AS
             S.visibility IS NOT NULL
             AND (S.current_flow_estimated IS NOT NULL
                  OR S.current_flow_instrumented IS NOT NULL)
-            AND sub.type IS NOT NULL
-            AND subc.name IS NOT NULL
+            AND sub.type IS NOT NULL  --substrate type
+            AND subc.name IS NOT NULL --substrate complexity
+            AND S.bait_id IS NOT NULL
+            AND (S.latitude!=0 AND S.longitude!=0)
             THEN 1
           ELSE 0
-          END  AS has_all_fields,
-          CASE
-          WHEN mas.id IS NOT NULL
-               AND S.visibility IS NOT NULL
-               AND (S.current_flow_estimated IS NOT NULL
-                    OR S.current_flow_instrumented IS NOT NULL)
-               AND sub.type IS NOT NULL
-               AND subc.name IS NOT NULL
-            THEN 1
-          ELSE 0
-          END  AS set_complete
+          END  AS has_all_set_metadata
         FROM trip_trip t
           INNER JOIN bruv_set S ON S.trip_id = t.id
           LEFT JOIN habitat_substrate sub ON sub.id = S.substrate_id
@@ -96,55 +61,46 @@ CREATE OR REPLACE VIEW public.v_report_reef_summary AS
           LEFT JOIN annotation_masterrecord mas ON (mas.set_id = S.id
                                                     AND mas.status_id = 2)
           LEFT JOIN assignment_status_summary assstat ON assstat.video_id = v.id
+          LEFT JOIN habitat_reefhabitat ON S.reef_habitat_id=habitat_reefhabitat.id
         GROUP BY
           S.trip_id,
           S.id,
           S.reef_habitat_id,
+          habitat_reefhabitat.reef_id,
           has_complete_master,
-          has_all_fields,
-          set_complete
+          has_all_set_metadata
     ),
-    annotation_summary AS (
+
+    set_summary_agg AS (
   SELECT
-    extract(YEAR FROM t.start_date) :: TEXT AS trip_year,
-    hab.region_name                         AS region_name,
-    hab.location_name                       AS location_name,
-    hab.site_name                           AS site_name,
-    hab.reef_name                           AS reef_name,
+      trip_id,
+      reef_id,
+      count(distinct set_id)    AS num_sets,
+sum(min_1_complete_annotation)    AS have_min_1_complete_annotation,
+sum(has_complete_master)          AS have_complete_master,
+sum(has_all_set_metadata)         AS have_all_set_metadata
+      FROM set_summary
+      GROUP BY reef_id,
+      trip_id
+    ),
 
-    count(DISTINCT s.set_id)                AS count_of_sets,
-    sum(s.has_assignments)                  AS with_assignments,
-    sum(s.has_two_complete_assignments)     AS have_two_complete_assignments,
-    sum(s.has_two_reviewed_assignments)     AS have_two_reviewed_assignments,
-    sum(s.one_assignment_one_reviewed)      AS have_one_assignment_and_one_reviewed,
-    sum(s.has_complete_master)              AS have_complete_master,
-    sum(s.has_all_fields)                   AS have_all_set_fields,
-    sum(s.set_complete)                     AS complete_sets,
-
-    hab.region_id,
-    hab.location_id,
-    hab.site_id,
-    hab.reef_id
-  FROM
-    trip_trip t
-    INNER JOIN set_summary s ON s.trip_id = t.id
-    INNER JOIN habitat_summary hab ON hab.reef_habitat_id = s.reef_habitat_id
-  GROUP BY
-    trip_year,
-    hab.region_name,
-    hab.location_name,
-    hab.site_name,
-    hab.reef_name,
-    hab.region_id,
-    hab.location_id,
-    hab.site_id,
-    hab.reef_id
-  ORDER BY
-    hab.region_name,
-    hab.location_name,
-    hab.site_name,
-    hab.reef_name,
-    trip_year),
+    habitat_summary AS (
+        SELECT DISTINCT
+          extract(YEAR FROM t.start_date) :: TEXT AS trip_year,
+          t.code                                  AS trip_code,
+          t.id                                    AS trip_id,
+          hab.region_name                         AS region_name,
+          hab.location_name                       AS location_name,
+          hab.site_name                           AS site_name,
+          hab.reef_name                           AS reef_name,
+          hab.region_id,
+          hab.location_id,
+          hab.site_id,
+          hab.reef_id
+        FROM
+          trip_trip t
+          INNER JOIN habitat_summary hab ON hab.location_id = t.location_id
+    ),
 
 
 reef_metadata AS (
@@ -179,6 +135,8 @@ reef_metadata AS (
       habitat_mpaisolation.type,
       habitat_mpacompliance.type
 ),
+
+
     fishing_metadata AS (
       SELECT
         habitat_reef.id                               AS reef_id,
@@ -189,36 +147,48 @@ reef_metadata AS (
       GROUP BY habitat_reef.id
   )
 
+
 SELECT
 region_name,
 location_name,
 site_name,
 reef_name,
 trip_year,
-count_of_sets,
-with_assignments,
-have_two_complete_assignments,
-have_two_reviewed_assignments,
-have_one_assignment_and_one_reviewed,
-have_complete_master,
-have_all_set_fields,
+trip_code,
+  CASE WHEN
+    num_sets IS NULL THEN 0
+    ELSE num_sets
+      END AS num_sets,
+  CASE WHEN have_min_1_complete_annotation IS NULL
+    THEN 0
+      ELSE have_min_1_complete_annotation
+        END AS have_min_1_complete_annotation,
+  CASE WHEN have_complete_master IS NULL
+    THEN 0
+      ELSE have_complete_master
+        END AS have_complete_master,
+  CASE WHEN have_all_set_metadata IS NULL
+    THEN 0
+      ELSE have_all_set_metadata
+        END AS have_all_set_metadata,
 protection_status,
 mpa_name,
-mpa_area,
-MPA_year_founded,
-MPA_isolation,
-MPA_compliance,
-fishing_restrictions,
-shark_fishing_gear,
+  mpa_area,
+  mpa_year_founded,
+  mpa_isolation,
+  mpa_compliance,
+  fishing_restrictions,
+  shark_fishing_gear,
 region_id,
 location_id,
+habitat_summary.trip_id
 site_id,
-annotation_summary.reef_id
-FROM
-annotation_summary
-LEFT JOIN reef_metadata ON annotation_summary.reef_id=reef_metadata.reef_id
-LEFT JOIN fishing_metadata ON annotation_summary.reef_id=fishing_metadata.reef_id;
+habitat_summary.reef_id
+  FROM habitat_summary
+       LEFT JOIN set_summary_agg ON set_summary_agg.reef_id=habitat_summary.reef_id
+                             AND   set_summary_agg.trip_id=habitat_summary.trip_id
+    LEFT JOIN reef_metadata ON reef_metadata.reef_id=habitat_summary.reef_id
+    LEFT JOIN fishing_metadata ON fishing_metadata.reef_id=habitat_summary.reef_id
 
-
-
+ ;
 
