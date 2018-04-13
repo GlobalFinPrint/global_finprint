114e8fd7fb1ce803c654a4cb385155a411c3514d
castic <castic@fiu.edu>
2018-04-13 16:30:04 -0400

    Fixed merge issue
    Fixed merge issue with reef_annotations_summary report

--- database/create_v_report_reef_annotations_summary.sql
+++ database/create_v_report_reef_annotations_summary.sql
@@ -1,19 +1,11 @@
 /* Summary of  completeness of annotations for all reefs */
 
 CREATE OR REPLACE VIEW public.v_report_reef_annotations_summary AS
-<<<<<<< HEAD
-  WITH assignment_status_summary AS
-  (
-      SELECT DISTINCT
-        s.id         AS set_id,
-        assig.video_id,
-=======
 /* Summary of  completeness of annotations for all reefs */
     WITH assignment_status_summary AS
   (
       SELECT DISTINCT
         s.id         AS set_id,
->>>>>>> master
         assigst.id   AS assignment_status_id,
         assigst.name AS assignment_status,
         count(*)     AS status_count
@@ -23,10 +15,6 @@ CREATE OR REPLACE VIEW public.v_report_reef_annotations_summary AS
         INNER JOIN annotation_annotationstate assigst ON assigst.id = assig.status_id
       GROUP BY
         s.id,
-<<<<<<< HEAD
-        assig.video_id,
-=======
->>>>>>> master
         assigst.id,
         assigst.name
   ),
@@ -54,19 +42,9 @@ CREATE OR REPLACE VIEW public.v_report_reef_annotations_summary AS
           END  AS has_complete_master
         FROM trip_trip t
           INNER JOIN bruv_set S ON S.trip_id = t.id
-<<<<<<< HEAD
-          LEFT JOIN habitat_substrate sub ON sub.id = S.substrate_id
-          LEFT JOIN habitat_substratecomplexity subc ON subc.id = S.substrate_complexity_id
-          LEFT JOIN annotation_video v ON v.id = S.video_id
-          LEFT JOIN annotation_videofile vf ON (vf.video_id = v.id AND vf."primary" = TRUE)
-          LEFT JOIN annotation_masterrecord mas ON (mas.set_id = S.id
-                                                    AND mas.status_id = 2)
-          LEFT JOIN assignment_status_summary assstat ON assstat.video_id = v.id
-=======
             LEFT JOIN annotation_masterrecord mas ON (mas.set_id = S.id
                                                     AND mas.status_id = 2)
           LEFT JOIN assignment_status_summary assstat ON assstat.set_id = S.id
->>>>>>> master
           LEFT JOIN habitat_reefhabitat ON S.reef_habitat_id=habitat_reefhabitat.id
         GROUP BY
           S.trip_id,
@@ -76,11 +54,7 @@ CREATE OR REPLACE VIEW public.v_report_reef_annotations_summary AS
           S.reef_habitat_id,
           habitat_reefhabitat.reef_id,
           has_complete_master
-<<<<<<< HEAD
-    ),
-=======
    ),
->>>>>>> master
 
    set_summary_agg AS (
   SELECT
@@ -96,15 +70,9 @@ sum(has_complete_master)          AS have_complete_master
       trip_id,
         trip_code,
         trip_year
-<<<<<<< HEAD
-    ),
-
-    habitat_summary AS (
-=======
    ),
 
    habitat_summary AS (
->>>>>>> master
         SELECT DISTINCT
           habitat_region.name                         AS region_name,
           habitat_location.name                       AS location_name,
@@ -120,11 +88,7 @@ sum(has_complete_master)          AS have_complete_master
           LEFT JOIN habitat_site ON habitat_location.id=habitat_site.location_id
           LEFT JOIN habitat_reef ON habitat_site.id=habitat_reef.site_id
           LEFT JOIN habitat_reefhabitat ON habitat_reef.id=habitat_reefhabitat.reef_id
-<<<<<<< HEAD
-      WHERE habitat_reef.id IS NOT NULL
-=======
      WHERE habitat_reef.id IS NOT NULL
->>>>>>> master
     )
 
 SELECT DISTINCT
@@ -157,8 +121,5 @@ habitat_summary.reef_id
  ;
 
 
-<<<<<<< HEAD
-=======
 
 
->>>>>>> master
