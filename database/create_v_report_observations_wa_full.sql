4dfba7c68f3995e9f47792f83e29ba1bdfc543f7
castic <castic@fiu.edu>
2018-04-30 09:46:52 -0400

    Reverted two reports
    Restored an old changeset of observations_wa and observations_wa_summary reports to help Elizabeth and Camila get the correct data.

---
+++ database/create_v_report_observations_wa_full.sql
@@ -0,0 +1,66 @@
+CREATE OR REPLACE VIEW public.v_report_observations_wa_full AS
+SELECT
+  tr.code                                                   AS trip_code,
+  s.code                                                    AS set_code,
+  tr.code || '_' || s.code                                  AS full_code,
+
+  hab.region_name                                                   AS region_name,
+  hab.location_name                                                 AS location_name,
+  hab.site_name                                                     AS site_name,
+  hab.reef_name                                                     AS reef_name,
+  hab.reef_habitat_name                                             AS reef_habitat_name,
+
+  u.first_name || ' ' || u.last_name                        AS annotator,
+
+  evt.event_time,
+  evt.event_time_minutes,
+
+  evt.observation_comment,
+  evt.event_note,
+
+  ani.family,
+  ani.genus,
+  ani.species,
+
+  evt.zero_time_tagged,
+  evt.sixty_minute_time_tagged,
+  evt.ninty_minute_time_tagged,
+  evt.haul_time_tagged,
+
+  evt.max_n_tagged,
+  evt.numeric_value_from_event_note,
+
+
+  tr.id                                                     AS trip_id,
+  s.id                                                      AS set_id,
+  hab.region_id,
+  hab.location_id,
+  hab.site_id,
+  hab.reef_id,
+  hab.reef_habitat_id,
+  v.id                                                      AS video_id,
+  a.id                                                      AS assignment_id,
+  ast.id                                                    AS assignment_state_id,
+  ast.name                                                  AS assignment_state
+FROM
+  trip_trip tr
+  INNER JOIN bruv_set s ON s.trip_id = tr.id
+
+  INNER JOIN habitat_summary hab ON hab.reef_habitat_id = s.reef_habitat_id
+
+  INNER JOIN annotation_video v ON v.id = s.video_id
+  INNER JOIN annotation_assignment a ON a.video_id = v.id
+  INNER JOIN annotation_annotationstate ast ON ast.id = a.status_id
+
+  INNER JOIN event_attribute_summary evt on evt.assignment_id = a.id
+
+  INNER JOIN core_finprintuser fu ON fu.id = evt.annotator_id
+  INNER JOIN auth_user u ON u.id = fu.user_id
+
+  LEFT JOIN annotation_animal ani ON ani.id = evt.animal_id
+
+WHERE hab.region_name = 'Western Atlantic'
+ORDER BY
+  full_code,
+  event_time,
+  annotator;
---
+++ database/create_v_report_observations_wa_summary_full.sql
@@ -0,0 +1,69 @@
+CREATE OR REPLACE VIEW public.v_report_observations_wa_summary_full AS
+WITH zero_times as
+(
+  select
+    set_id,
+    video_id,
+    max(
+        case
+          when zero_time_tagged = 0
+          then 0
+          else event_time
+        end) as zero_time
+  from event_attribute_summary
+  group by set_id,
+    video_id
+)
+SELECT
+  evtsum.trip_code,
+  evtsum.set_code,
+  evtsum.trip_code || '_' || evtsum.set_code                   AS full_code,
+
+  hab.region_name                                                   AS region_name,
+  hab.location_name                                                 AS location_name,
+  hab.site_name                                                     AS site_name,
+  hab.reef_name                                                     AS reef_name,
+  hab.reef_habitat_name                                             AS reef_habitat_name,
+
+  ani.family,
+  ani.genus,
+  ani.species,
+
+  min(evtsum.event_time_minutes)                               AS event_time_minutes_raw,
+  min(public.text_time(evtsum.event_time - z.zero_time))       AS event_time_minutes,
+  max(evtsum.numeric_value_from_event_note)                    AS maxn,
+
+  evtsum.trip_id,
+  evtsum.set_id,
+  evtsum.video_id
+FROM
+  event_attribute_summary evtsum
+  inner join zero_times z on z.video_id = evtsum.video_id
+  INNER JOIN habitat_summary hab ON hab.reef_habitat_id = evtsum.reef_habitat_id
+
+  INNER JOIN core_finprintuser fu ON fu.id = evtsum.annotator_id
+  INNER JOIN auth_user u ON u.id = fu.user_id
+
+  INNER JOIN annotation_animalobservation aobs ON aobs.observation_id = evtsum.observation_id
+  INNER JOIN annotation_animal ani ON ani.id = aobs.animal_id
+WHERE hab.region_name = 'Western Atlantic'
+    and evtsum.numeric_value_from_event_note is not null
+    and evtsum.max_n_tagged = 1
+GROUP BY
+  trip_code,
+  set_code,
+  full_code,
+  region_name,
+  location_name,
+  site_name,
+  reef_name,
+  reef_habitat_name,
+  family,
+  genus,
+  species,
+  evtsum.trip_id,
+  evtsum.set_id,
+  evtsum.video_id
+ORDER BY
+  full_code,
+  event_time_minutes_raw;
