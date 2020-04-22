ce5cc430ae8b3b22ed6fd46e17d37750b442be67
Orlando Rodriguez (CASE) <orlrodri@fiu.edu>
2020-04-22 16:47:25 -0400

    More logger testing...

--- global_finprint/report/views.py
+++ global_finprint/report/views.py
@@ -38,9 +38,20 @@ class StandardReportView(UserAllowedMixin, View):
     template = 'pages/reports/standard_report.html'
 
     def get(self, request, report, limit=None):
-        logging.basicConfig(filename="test.log", level=logging.DEBUG)
-        logging.error('Testing')
-        logging.error('%s', report)
+        logger = logging.getLogger('test_logger')
+        logger.setLevel(logging.DEBUG)
+        fh = logging.FileHandler('test.log')
+        fh.setLevel(logging.DEBUG)
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        ch.setFormatter(formatter)
+        logger.addHandler(fh)
+        logger.addHandler(ch)
+
+        logging.info('Testing')
+        logging.info('%s', report)
         report = Report(report)
         if not limit:
             limit = 'all'
