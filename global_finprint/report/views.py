f8744a5e09a34e38b4480bc891887cfddc8e9277
Orlando Rodriguez (CASE) <orlrodri@fiu.edu>
2020-04-22 16:58:44 -0400

    Removed logger code

--- global_finprint/report/views.py
+++ global_finprint/report/views.py
@@ -38,19 +38,6 @@ class StandardReportView(UserAllowedMixin, View):
     template = 'pages/reports/standard_report.html'
 
     def get(self, request, report, limit=None):
-        logger = logging.getLogger('test_logger')
-        logger.setLevel(logging.DEBUG)
-        fh = logging.FileHandler('test.log')
-        fh.setLevel(logging.DEBUG)
-        ch = logging.StreamHandler()
-        ch.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        fh.setFormatter(formatter)
-        ch.setFormatter(formatter)
-        logger.addHandler(fh)
-        logger.addHandler(ch)
-        logging.info('Testing')
-        logging.info('%s', report)
         report = Report(report)
         if not limit:
             limit = 'all'
